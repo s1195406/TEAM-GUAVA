4bab5c63ea6ba70b51d27e3b3ddcbdae9346b903
s1195406 <s1195406@student.mcckc.edu>
2026-09-04 11:40:17 -0500

    Add files via upload

---
+++ risignup&login (2).js
@@ -0,0 +1,69 @@
+<html>
+<html lang="en">
+<head>
+<title>Login to My Account></title>
+<style> body {background-color: Blue; Color: White;}
+<body>
+<header>"Login to your account"</header>
+<style> nav {section id: horizontal-top"}
+</style>
+<nav>
+<ol>
+ <h1><a href="home.html">Home</a></li></h1>
+ <h2><a href="signup.html">Signup</a></li></h2>
+<script>
+<form>
+<style> header {section id: vertical-centered;}
+               form  { min-width: 80em; }
+               input { margin-bottom: 1em; }
+               label { width: 5em; padding-right: 1em;   text-align: centered; }
+               submit { margin-middle: 5em; }	
+               agree {font-family: Arial, sans-serif; font-size: .90em; color: #FF0000;  margin-centered1em;
+               $('document').ready('function')
+               $('form').accept('login');
+</script>
+<div>
+  <form>
+    <h3><a href="sign up&login.html"></a></h3>
+<form method="post" id="Signup Form"
+      action="https://clemonsrealestate.com/scripts/signup.js">>
+<div>
+<label for="ownerName">Name:</label>
+<input type="text" name="ownerName" id="ownerName" class="required">
+</div>
+<div>
+<label for="ownerEmail">E-mail:</label>
+<input type="text" name="ownerEmail" id="ownerEmail" class="required email">
+</div>
+<div>
+<label for="ownerPassword">Password:</label>
+<input type="text" password="ownerPassword" id="ownerPassword" class="required password">
+</div>
+<div>
+<label for="confirmPassword">confirmPassword:</label>
+<input type="text" password="confirmPassword" id="confirmPassword" class="required confirmpassword">
+</div>
+<div>
+<label for="Agree to terms of Service">class="required Agree to Terms">
+<button for="Continue">Continue</button>
+</div>
+</form>
+    <h4><a href="login.html">Login</a></h4>
+<div>
+  <label for="userEmail">E-mail:</label>
+   <input type="text" name="userEmail" id="userEmail" class="required email">
+</div>
+<div>
+   <label for="userPassword">Password:</label>
+   <input type="text" password="userPassword" id="userPassword" class="required password">
+   </div>
+   <div>
+   <button for="continue:" >"Continue:</button
+   <input type="check" password="check" id="check" class="required agree to terms">
+</div>
+</form>
+</script>
+    <h5><a href="owneraccount.html>>Owner Account</a></li></h5>
+</body>
+</style>
+</html>
---
+++ risignup&login.js
@@ -0,0 +1,28 @@
+<html>
+<title>The Greystone Apartments Tenant Sign Up & Login</title>
+<head>
+<script>
+  <script src="https://www.greystoneapartments.com/scripts/signupandlogin.js">
+  <script validate/1.19.0/validate.js"></script>
+<script>
+ <script>  
+
+<ul><li>function validateform(){  </li></ul>
+<ul><li>var <font color="#ff0000">name</font>=<font color="#0000ff">document</font>.myform.name.value;  </li></ul>
+<ul><li>var <font color="#ff0000">password</font>=<font color="#0000ff">document</font>.myform.password.value;  </li></ul>
+<ul><li>  </li></ul>
+<ul><li>if (<font color="#ff0000">name</font>==null || <font color="#ff0000">name</font>==""){  </li></ul>
+<ul><li>  alert("Name can't be blank");  </li></ul>
+<ul><li>  return false;  </li></ul>
+<ul><li>}else if(password.length<6){  </li></ul>
+<ul><li>  alert("Password must be at least 6 characters long.");  </li></ul>
+<ul><li>  return false;  </li></ul>
+<ul><li>  }  </li></ul>
+<ul><li>}  </li></ul>
+<ul><li></script>  </li></ul>
+<ul><li><body>  </li></ul>
+<ul><li><form <font color="#ff0000">name</font>=<font color="#0000ff">"myform"</font> <font color="#ff0000">method</font>=<font color="#0000ff">"post"</font> <font color="#ff0000">action</font>=<font color="#0000ff">"abc.jsp"</font> <font color="#ff0000">onsubmit</font>=<font color="#0000ff">"return validateform()"</font> >  </li></ul>
+<ul><li>Name: <input <font color="#ff0000">type</font>=<font color="#0000ff">"text"</font> <font color="#ff0000">name</font>=<font color="#0000ff">"name"</font>><br/>  </li></ul>
+<ul><li>Password: <input <font color="#ff0000">type</font>=<font color="#0000ff">"password"</font> <font color="#ff0000">name</font>=<font color="#0000ff">"password"</font>><br/>  </li></ul>
+<ul><li><input <font color="#ff0000">type</font>=<font color="#0000ff">"submit"</font> <font color="#ff0000">value</font>=<font color="#0000ff">"register"</font>>  </li></ul>
+<ul><li></form>  </li></ul><div><br/><br/></script><br/></head><br/><br/><body><br/><style> body {background-color:Red; color: White;input-margin-bottom: 1em;padding-right: 1em;text-align: center;}</style><br/><nav><br/> <ul><br/> <li><a href="index.html">Index</a></li></ul><br/><style> nav ul li {nav id="horizontal;" margin: top-left; background-color: DarkGray; color: DarkRed; font-size:48px; text-align: center;}</style><br/>  <ol><br/><h1><a href="home.html">Home</a></h1><br/><h2><a href="photos.png">Photos</a></h2><br/><h3><a href="apply.html">Apply</a></h3><br/> <h4><a href="signup.html">Signup</a></h4><br/><h5><a href="login.html">Login</a></h5><br/><h6><a href="signupandlogin.html">SignupandLogin</h6><br/><style> ol h1 h2 h3 h4 h5 {background-color:Red; color: White;input-margin-bottom: 1em;padding-right: 1em;text-align: center;}</style><br/>    <img src="login.png" img with="65%" img height="85%" img rel="IMG" img align: centered:><br/> </ol><br/></nav><br/><header>Dont have an account? Signup Here</header><br/><style> header {background-color: DarkRed; color: White; text-align: centered;}</style><br/><form><br/><style> form {background-color: DarkGray; min-width: 80em;}</style><br/><div><br/><label for="tenantName">Name:</label><br/><style> label {label-width: 5em;}</style><br/><input type="text" name="tenantName" id="tenantName" class="required"><br/></div><br/><div><br/><label for="tenantEmail">E-mail:</label><br/><input type="text" name="tenantEmail" id="tenantEmail" class="required email"><br/></div><br/><div><br/><label for="tenantPassword">Password:</label><br/><input type="text" password="tenantPassword" id="tenantPassword" class="required password"><br/></div><br/><input type="submit" value="Signup" id="submit"><br/><style> submit  margin-left: 5em; font-family: Arial, sans-serif: font-size: .90px;margin-left: 1em;}</style><br/><button for="continue">Continue</button><br/></form><br/></div><div></div><header>Already have an account? Login Here</header><br/><style> header {background-color: DarkRed; color: White;}</style><br/><form><br/><style> form {background-color: DarkGray; min-width: 80em;}</style><br/><div><br/><label for="tenantEmail">E-mail</label><br/><style> label {label-width: 5em;}</style><br/><input type="text" name="tenantEmail" id="tenantEmail" class="required email"><br/></div><br/><div><br/><label for="userPassword">Password:</label><br/><input type="text" password="tenantPassword" id="tenantPassword" class="required password"><br/><div><br/><label for="confirmPassword">confirmPassword:</label><br/><input type="text" password="confirmPassword"><br/></div><br/><div><br/><button for="continue">Continue</button><br/></div><br/><style> div {margin-left: 5em; font-family: Arial, sans-serif: font-size:.90px;margin-left: 1em;}</style><br/></div><br/></form><br/></div><br/></body><div></div><footer>The Greystone Apartments 124 West Linwood Blvd Kansas City, MO 64111</footer><br/><style> footer{background-color: DarkRed; font-color: White; text-align: bottom-centered;}</style><br/></html><br/></div>
